c6cf577438ea69d0b30fd75e4bcaaaa97815bb73
Volinger <tisovskyerik@gmail.com>
2023-03-20 18:26:38 +0100

    added url conf for staticfiles

--- Stats/urls.py
+++ Stats/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path, include
 from . import views
 from rest_framework.routers import DefaultRouter
+from django.conf import settings
+from django.conf.urls.static import static
 
 router = DefaultRouter()
 router.register(r'', views.StatsViewSet, basename="Stats")
 
 urlpatterns = [
     path('', include(router.urls)),
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
