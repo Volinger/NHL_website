82a18a29de945f3ecb700a5a4789b072b9de4cf2
Volinger <tisovskyerik@gmail.com>
2023-03-30 00:03:58 +0200

    added missing logging cfg

--- NHL_website/production_settings.py
+++ NHL_website/production_settings.py
@@ -144,3 +144,21 @@ STATIC_ROOT = BASE_DIR / "staticfiles/static"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGGING = {
+    'version': 1,                       # the dictConfig format version
+    'disable_existing_loggers': False,  # retain the default loggers
+    'handlers': {
+        'file': {
+            'class': 'logging.FileHandler',
+            'filename': '/home/site/wwwroot/general.log',
+            'level': 'INFO',
+        },
+    },
+    'loggers': {
+        '': {
+            'level': 'INFO',
+            'handlers': ['file'],
+        },
+    },
+}
